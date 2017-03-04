630593ada60e167c7ad1afc592540c9e27d89038
David <dmartinezmillan@hotmail.com>
2017-03-04 21:49:20 +0100

    Added Remove Tab

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,6 @@
 function newTab(e) {
 	chrome.tabs.create({});
-	console.log("New tab created!")
+	console.log("New tab created")
 }
 
 function refreshing(e) {
@@ -8,8 +8,16 @@ function refreshing(e) {
 	console.log("Refresh")
 }
 
+function removeTab(e) {
+	chrome.tabs.getSelected(null, function(tab){
+    	chrome.tabs.remove(tab.id)
+    	console.log("The current tab was removed")
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById("cmd_newtab").addEventListener('click', newTab);
 	document.getElementById("cmd_refresh").addEventListener('click', refreshing);
+	document.getElementById("cmd_removetab").addEventListener('click', removeTab);
 });
 
