de9bcb205f11640144e6723352b270c1df3b7fb2
David <dmartinezmillan@hotmail.com>
2017-03-05 00:24:57 +0100

    Added Go Back

--- extension/popup.js
+++ extension/popup.js
@@ -5,7 +5,7 @@ function newTab(e) {
 
 function refreshing(e) {
 	chrome.tabs.reload()
-	console.log("Refresh")
+	console.log("Refreshed")
 }
 
 function removeTab(e) {
@@ -15,9 +15,22 @@ function removeTab(e) {
 	});
 }
 
+function goBack(e){
+	var microsecondsADay = 1000 * 60 * 60 * 24;
+  	var today = (new Date).getTime() - microsecondsADay;
+	chrome.history.search({'text': '', 'startTime': today }, function(historyItems){
+    	var lastUrl = historyItems[1].url;
+    	chrome.tabs.getSelected(null, function(tab){
+    		chrome.tabs.update(tab.id, {url: lastUrl})
+    		console.log("Went back")
+		});
+    	
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById("cmd_newtab").addEventListener('click', newTab);
 	document.getElementById("cmd_refresh").addEventListener('click', refreshing);
 	document.getElementById("cmd_removetab").addEventListener('click', removeTab);
+	document.getElementById("cmd_goBack").addEventListener('click', goBack);
 });
-
