413e892e5ec782a33176d063c86172f809fd6384
David <dmartinezmillan@hotmail.com>
2017-03-05 01:34:43 +0100

    Added Twitter and Newspaper Openings

--- extension/popup.js
+++ extension/popup.js
@@ -27,9 +27,25 @@ function goBack(e){
 	});
 }
 
+function openTwitter(e){
+	chrome.tabs.getSelected(null, function(tab){
+    	chrome.tabs.update(tab.id, {url: "https://twitter.com/"})
+    	console.log("Twitter opened")
+	});
+}
+
+function openNewspaper(e){
+	chrome.tabs.getSelected(null, function(tab){
+    	chrome.tabs.update(tab.id, {url: "https://www.theguardian.com/"})
+    	console.log("Newspaper opened")
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById("cmd_newtab").addEventListener('click', newTab);
 	document.getElementById("cmd_refresh").addEventListener('click', refreshing);
 	document.getElementById("cmd_removetab").addEventListener('click', removeTab);
 	document.getElementById("cmd_goBack").addEventListener('click', goBack);
+	document.getElementById("cmd_twitter").addEventListener('click', openTwitter);
+	document.getElementById("cmd_newspaper").addEventListener('click', openNewspaper);
 });
