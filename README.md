2da1cefc076a7a46ee086e86843e5bf8094cd10f
Tim Nederveen <tim.nederveen@hotmail.com>
2017-03-10 21:01:12 +0100

    Google search result link highlighting: actual results now numbered 1-10

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Chrome Control allows you to control your browser and navigate solely by voice, 
 - tab history traversal
 - link highlighting
 - selecting and following links
-- opening and closing of tabs
+- opening and closing of tabs	
 - scrolling
 - directly loading popular websites
 - refreshing
@@ -17,4 +17,5 @@ TODO:
 - Include login by amazon account ID on plugin side, to match installed skills to installed plugins
 - Expand intents and sample utterances for better and more natural query recognition
 - Web dashboard for users to add their own preferred sites as shortcuts
-- 
+- Implement additional features such as opening favourites and filling in search boxes
+- Inject [Web Speech API](https://developers.google.com/web/updates/2013/01/Voice-Driven-Web-Apps-Introduction-to-the-Web-Speech-API) for filling in forms or search boxes () 
--- extension/highlightLinks.js
+++ extension/highlightLinks.js
@@ -5,6 +5,8 @@ function getElementByXpath(path) {
 var currentUrl = window.location.href;
 
 if(currentUrl.includes('google')) {
+	/*
+
 	var css = ".r {overflow:visible !important; z-index: 0;position: relative;}.r:before {z-index: 1;display: inline-block;position: absolute;left: -2rem;bottom: 1.2rem;margin: 0 0.5rem 0 0;width: 1.5rem;height: 1.5rem;font-size: 0.9rem;line-height: 1.5rem;text-align: center;background-color: rgba(255, 255, 0, 0.5);border: solid 2px rgba(0, 0, 0, 0.5);content: attr(data-index);}";
 	// var css = css.concat("a {overflow:visible !important; z-index: 0;position: relative;}a:before {z-index: 1;display: inline-block;position: absolute;left: -2rem;bottom: 1.2rem;margin: 0 0.5rem 0 0;width: 1.5rem;height: 1.5rem;font-size: 1.2rem;opacity: 0.5;line-height: 1.5rem;text-align: center;background-color: #ff0;border: solid 2px #000;content: attr(data-index);}");
 	
@@ -16,6 +18,24 @@ if(currentUrl.includes('google')) {
 	  //getElementByXpath(`//*[@id=\"rso\"]/div/div/div[${i}]/div/h3/a`).style.backgroundColor = "yellow";
 	  getElementByXpath(`//*[@id=\"rso\"]/div/div/div[${i}]/div/h3`).setAttribute("data-index", i);
 	  i++;
+	}
+
+	*/
+
+	var css = "h3.r {overflow:visible !important} a:not(.ab_button) {overflow:visible !important; z-index: 0;position: relative;} a:not(.ab_button):before {z-index: 1;display: inline-block;position: absolute;left: -2rem;bottom: 1.2rem;margin: 0 0.5rem 0 0;width: 1.5rem;height: 1.5rem;font-size: 0.9rem;line-height: 1.5rem;text-align: center;background-color: rgba(255, 255, 0, 0.5);border: solid 2px rgba(0, 0, 0, 0.5);content: attr(data-index);}";
+	var links = document.querySelectorAll("a:not(.ab_button)"); //document.getElementsByTagName('a');
+	var number = 15
+	for(var i = 0; i< links.length; i++){
+	  links[i].setAttribute("data-index", number++);
+	  // links[i].style.backgroundColor = "yellow";
+	}
+
+	// second loop to give all result links a low data-attribute number of 1-14
+	number = 1;
+	var links = document.querySelectorAll("div>h3.r>a:not(.ab_button)"); //document.getElementsByTagName('a');
+	for(var i = 0; i< links.length; i++){
+	  links[i].setAttribute("data-index", number++);
+	  // links[i].style.backgroundColor = "yellow";
 	}
 } else {
 	var css = "a {overflow:visible !important; z-index: 0;position: relative;}a:before {z-index: 1;display: inline-block;position: absolute;left: -2rem;bottom: 1.2rem;margin: 0 0.5rem 0 0;width: 1.5rem;height: 1.5rem;font-size: 0.9rem;line-height: 1.5rem;text-align: center;background-color: rgba(255, 255, 0, 0.5);border: solid 2px rgba(0, 0, 0, 0.5);content: attr(data-index);}";
